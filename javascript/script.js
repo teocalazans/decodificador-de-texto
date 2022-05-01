1e6fdd3573a4d600204dbace102cb0fb7120b782
teocalazans <teocalazans20@gmail.com>
2022-04-30 22:08:19 -0300

    Construção da lógica para Criptografia e Descriptografia + conclusão do projeto

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,38 @@
-var botaoCript = document.querySelector(".botao-cript")
-console.log("clicado")
+var botaoCript = document.querySelector(".botao-cript");
+var botaoDescript = document.querySelector(".botao-descript");
+var campoTexto = document.querySelector(".caixa-texto").value;
+botaoCript.innerHTML = campoTexto;
+
+
+var esconde = document.querySelector(".second-box-section-result")
+esconde = document.querySelector(".second-box-section-result").style.visibility = "hidden"
+
+botaoCript.addEventListener("click", function () {
+    var campoTexto = document.querySelector(".caixa-texto").value;
+    var campoResultado = document.querySelector("#resultado");
+
+    var esconde = document.querySelector(".second-box-section-result")
+    esconde = document.querySelector(".second-box-section-result").style.visibility = "visible";
+
+    var resultadoCriptografia = campoTexto.replace(/[a]/gi, "ai").replace(/[e]/gi, 'enter').replace(/[i]/gi, 'imes').replace(/[o]/gi, 'ober').replace(/[u]/gi, 'ufat');
+    campoResultado.value = resultadoCriptografia
+});
+
+botaoDescript.addEventListener("click", function () {
+    var campoTexto = document.querySelector(".caixa-texto").value;
+    var campoResultado = document.querySelector("#resultado");
+
+    var esconde = document.querySelector(".second-box-section-result")
+    esconde = document.querySelector(".second-box-section-result").style.visibility = "visible";
+
+    var resultadoDescriptografia = campoTexto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    campoResultado.value = resultadoDescriptografia
+})
+
+var copiarTexto = document.querySelector("#alinhamento-botao")
+
+copiarTexto.addEventListener("click", function(e){
+    var textArea = document.querySelector("#resultado")
+    textArea.select()
+    document.execCommand("Copy")
+})
